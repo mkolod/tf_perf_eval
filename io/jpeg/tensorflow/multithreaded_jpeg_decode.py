5641cfdb252aaed1c44c5fe818862adf45ea9ad3
Marek Kolodziej <mkolod@gmail.com>
2016-11-01 14:00:50 -0700

    TensorFlow JPEG decode

--- io/jpeg/tensorflow/multithreaded_jpeg_decode.py
+++ io/jpeg/tensorflow/multithreaded_jpeg_decode.py
@@ -1,27 +1,16 @@
-from datetime import datetime
-import multiprocessing
-import os
-from os import listdir
-from os.path import isfile, join
 from time import time
-
+import multiprocessing 
 import numpy as np
-
 import tensorflow as tf
 
-proto_in = '/datasets/resized_imagenet/training/'
-# proto_in = '/datasets/imagenet_original'
-only_files = [f for f in listdir(proto_in) if isfile(join(proto_in, f))]
-
-# make sure we have 1,024 training and 128 validation proto files
-only_train_files = [os.path.join(proto_in, f) for f in only_files if "train" in f]
-only_validation_files = [os.path.join(proto_in, f) for f in only_files if "valid" in f]
-
-print("number of training files: %d" % len(only_train_files))
-print("number of validation files: %d" % len(only_validation_files))
+num_repeat = 100000
+file_list = ['/tf_jpeg_perf/ElCapitan_256_by_256.jpg'] * num_repeat
 
 num_inter_threads = multiprocessing.cpu_count() / 2
 num_intra_threads = multiprocessing.cpu_count() / 2
+
+print("Running multi-threaded JPEG decode in TensorFlow. Please wait.\n")
+
 
 feature_map = {
 'image/encoded': tf.FixedLenFeature([], dtype=tf.string,
@@ -42,11 +31,9 @@ sess = tf.Session(config=tf.ConfigProto(
     inter_op_parallelism_threads=num_inter_threads,
     intra_op_parallelism_threads=num_intra_threads))
 
-batch_size = 1024
+batch_size = 1000
 
-data_files = only_train_files[0:100]
-
-filename_queue = tf.train.string_input_producer(string_tensor = data_files,
+filename_queue = tf.train.string_input_producer(string_tensor = file_list,
                                                     shuffle = False,
                                                     seed = 42,
                                                     capacity = num_intra_threads * batch_size,
@@ -58,7 +45,8 @@ sess.run(tf.initialize_local_variables())
 
 reader_coord = tf.train.Coordinator()
 
-reader = tf.TFRecordReader()
+reader = tf.WholeFileReader()
+
 _, read_op = reader.read(filename_queue)
 
 examples_queue = tf.FIFOQueue(
@@ -76,9 +64,8 @@ processed_tensor_tuples = []
 for thread in xrange(num_inter_threads):
         
     ex_dequeue_op = examples_queue.dequeue()
-    proto_parse_op = tf.parse_single_example(ex_dequeue_op, feature_map)
     
-    encoded_image = proto_parse_op['image/encoded']
+    encoded_image = ex_dequeue_op
     decoded = tf.image.decode_jpeg(encoded_image, channels = 3)  
     
     # dummy op to force use of jpeg decoding
@@ -102,7 +89,7 @@ try:
         
         
 except tf.errors.OutOfRangeError:
-    print('Done')
+    pass
 finally:
     reader_coord.request_stop()
     process_coord.request_stop()
@@ -114,6 +101,9 @@ sess.close()
 
 end = time()
 total_time = end - start
+im_sec = float(count) / total_time
 
 print("Running time: %.2f seconds" % total_time)
-print("Images/second: %.2f" % (float(count) / total_time))
+print("Read %d images" % count)
+print("Images/second: %.2f" % im_sec)
+print("Images/second/core: %.2f" % (im_sec / num_inter_threads))
